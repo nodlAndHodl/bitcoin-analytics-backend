021df499b76cfc350b48176b7e6337d65a0ec723
nick shoup <shoupnb@gmail.com>
2024-08-14 16:58:12 -0600

    adding in .env.example

--- main.go
+++ main.go
@@ -13,21 +13,6 @@ var (
 
 func main() {
 	defer config.DisconnectDB(db)
-	//run all migrations
-	// bitcoindService, err := bitcoind.NewBitcoindService()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// blockhash, err := bitcoindService.GetBlockHash(0)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Block: %v\n", blockhash)
-	// block, err := bitcoindService.GetBlock(blockhash)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Block: %v\n", block)
 	models.AutoMigrate()
 	//run all routes
 	routes.Routes()
